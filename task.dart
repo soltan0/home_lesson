243e17e75e3571cd7378a393a4444b47f1616e80
soltan0 <1210606616@nku.edu.tr>
2023-11-20 20:03:31 +0400

    home_lesson

--- task.dart
+++ task.dart
@@ -1,14 +1,33 @@
 import 'dart:io';
 
-void main(List<String> args) {
+
+void main() {
   String nameMarket = 'best buy';
   print(nameMarket);
-  stdout.writeln('adinzi daxil edin');
+
+  String electronics1 = 'telephone';  
+  String electronics2 = 'noutbook';
+  String electronics3 = 'playstation';
+
+  stdout.writeln('Adiniz daxil edin');  // Fix the spelling of "Adiniz."
   String name = stdin.readLineSync().toString();
   print('Adiniz $name');
-  stdout.writeln('meshet elektronika \n1-telefon \n2-noutbook \n3-playstation');
+
+  stdout.writeln('electronics \n1-telefon \n2-noutbook \n3-playstation');
   String choice = stdin.readLineSync().toString();
-  switch (1) {
-    case ('\$50'):
-  }
+ 
+ if (choice == '1') {
+    print('You selected $electronics1');
+  } else if (choice == '2') {
+    print('You selected $electronics2');
+  } else if (choice == '3') {
+    print('You selected $electronics3');
+  } else {
+    print('Invalid choice');
+ }
+String Cash = "yes";
+String Cart = "yes";
+ stdout.writeln('chose \n1-nagd \n2- kart ');
+
 }
+
